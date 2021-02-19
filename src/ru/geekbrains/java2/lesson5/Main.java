f6144e58a0dd37e514ad045799f61ba3ada6f3fd
Novikova-EY <75306313+Novikova-EY@users.noreply.github.com>
2021-02-19 16:07:47 +0300

    HomeWork_Java_Level_2-Lesson_5

---
+++ src/ru/geekbrains/java2/lesson5/Main.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.java2.lesson5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConcurrencyCalc calc = new ConcurrencyCalc();
+        calc.doFirstCase();
+        calc.doSecondCase();
+    }
+}
